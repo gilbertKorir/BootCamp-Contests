77dda9bd8fd1d57c489d8b3856ec182b850da2f1
gilbertKorir <jerikorir@gmail.com>
2022-05-28 10:39:36 +0300

    ft:implemented the view on clicklistener interface

--- app/src/main/java/com/example/hackathoncontests/LoginActivity.java
+++ app/src/main/java/com/example/hackathoncontests/LoginActivity.java
@@ -2,11 +2,21 @@ package com.example.hackathoncontests;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.submitButton) Button mSubmitButton;
+    @BindView(R.id.nameEditText) EditText mName;
+    @BindView(R.id.schoolEditText) EditText mSchool;
+    @BindView(R.id.passwordEditText) EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +24,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
         getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mSubmitButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        if(v == mSubmitButton){
+            Intent intent = new Intent(LoginActivity.this, ContestsActivity.class);
+            String name = mName.getText().toString();
+            String school = mSchool.getText().toString();
+            String
+        }
 
     }
 }
