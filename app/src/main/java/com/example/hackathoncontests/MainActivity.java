2000838cdfd14e9bfd51466e6216846e4244b63a
gilbertKorir <jerikorir@gmail.com>
2022-05-28 09:22:06 +0300

    ft: called the registration button with intent

--- app/src/main/java/com/example/hackathoncontests/MainActivity.java
+++ app/src/main/java/com/example/hackathoncontests/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.hackathoncontests;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.registerButton) Button mRegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
         getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
