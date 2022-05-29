eb823b636fdae32cee141fe8e2a17fa30e06a810
gilbertKorir <jerikorir@gmail.com>
2022-05-29 20:42:13 +0300

    ft:generated a login instrumented test

---
+++ app/src/androidTest/java/com/example/hackathoncontests/LoginActivityInstrumentationTest.java
@@ -0,0 +1,28 @@
+package com.example.hackathoncontests;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class LoginActivityInstrumentationTest {
+    @Rule
+    public ActivityScenarioRule<LoginActivity> activityRule = new ActivityScenarioRule<>(LoginActivity.class);
+
+    @Test
+    public void locationIsSentToRestaurantsActivity() {
+        String name = "Korir";
+        String school = "Moringa";
+        String password = "korir12";
+        onView(withId(R.id.nameEditText)).perform(typeText(name));
+        onView(withId(R.id.schoolEditText)).perform(typeText(school));
+        onView(withId(R.id.passwordEditText)).perform(typeText(password));
+        onView(withId(R.id.submitButton)).perform(click());
+        onView(withId(R.id.locationTextView)).check(matches(withText("Here are all the restaurants near: " + location)));
+    }
+}
