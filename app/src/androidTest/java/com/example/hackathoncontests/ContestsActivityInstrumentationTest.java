a78fbfbfc7891e96fd83d25fe2b037cbb7f8f75b
gilbertKorir <jerikorir@gmail.com>
2022-05-31 12:21:46 +0300

    ft:configured the login test when submit is clicked

---
+++ app/src/androidTest/java/com/example/hackathoncontests/ContestsActivityInstrumentationTest.java
@@ -0,0 +1,24 @@
+package com.example.hackathoncontests;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ContestsActivityInstrumentationTest {
+    @Rule
+    public ActivityScenarioRule<ContestsActivity> activityRule = new ActivityScenarioRule<>(ContestsActivity.class);
+
+    @Test
+    public void confirmNameandSchool() {
+        String name = "korir";
+        String school = "Moringa";
+        onView(withId(R.id.viewName)).check(matches(withText("Your Name: "+ name)));
+        onView(withId(R.id.viewSchool)).check(matches(withText("Your School: " + school)));
+    }
+    }
